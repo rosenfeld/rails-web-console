34f3ffc4545f35fe748bc324319f808b46d00961
Rodrigo Rosenfeld Rosas <rodrigo@geocontrol.com.br>
2010-11-22 09:40:10 -0200

    Changes clear shortcut to "Esc, Esc" because Ctrl+Del has side effects

--- public/javascripts/console.js
+++ public/javascripts/console.js
@@ -1,3 +1,4 @@
+var lastEscTime = 0
 jQuery(function($) {
     $('#clearResults').click(function() { $('#results').empty() })
     $('#runScript').click(function() {
@@ -7,6 +8,9 @@ jQuery(function($) {
     })
     $('#script').keydown(function(ev) {
         if (ev.ctrlKey && ev.keyCode == 13) $('#runScript').click() // Ctrl + Enter
-        if (ev.ctrlKey && ev.keyCode == 46) $('#clearResults').click() // Ctrl + Delete
+        if (ev.keyCode == 27) {
+            if (new Date().getTime() - lastEscTime < 1000) $('#clearResults').click() // Esc, Esc within a second
+            lastEscTime = new Date().getTime()
+        }
     })
 })
