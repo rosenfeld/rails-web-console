05da310491e8f8adb3ad2885eaa9f5b6d7b052ec
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2011-07-14 21:48:00 -0300

    Escapes results sent back to the browser

--- app/controllers/console_controller.rb
+++ app/controllers/console_controller.rb
@@ -10,11 +10,17 @@ class ConsoleController < ApplicationController
     begin
       result_eval = eval params[:script], binding
       $stdout.rewind
-      result = %Q{<div class="stdout">#{$stdout.read}</div><div class="return">#{result_eval.inspect}</div>}
+      result = %Q{<div class="stdout">#{escape $stdout.read}</div>
+        <div class="return">#{escape result_eval.inspect}</div>}
     rescue Exception => e
       result = e.to_s
     end
     $stdout = stdout_orig
     render text: result.gsub("\n", "<br />\n")
+  end
+
+private
+  def escape(content)
+    view_context.escape_once content
   end
 end
